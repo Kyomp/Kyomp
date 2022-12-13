63aab7634f27f7665ae7abb05b44eb74d1649a12
Kyomp <100985495+Kyomp@users.noreply.github.com>
2022-12-14 01:26:19 +0700

    Improved the logic

--- MineSweeper/MineSweeper.py
+++ MineSweeper/MineSweeper.py
@@ -29,7 +29,7 @@ possibleSpace = set(possible)
 topLeft = possible[0]
 bottomRight = possible[-1]
 reg = (topLeft[0]-10, topLeft[1]-10, bottomRight[0]-topLeft[0]+26, bottomRight[1]-topLeft[1]+26)
-flagged = set(list(pyautogui.locateAllOnScreen('Flagged.png', confidence=0.8, region=reg)))
+flagged = set(pyautogui.locateAllOnScreen('Flagged.png', confidence=0.8, region=reg))
 safe = set()
 a = pyautogui.locateOnScreen('start.png', confidence=0.8, region=reg)
 if len(flagged) == 0:
@@ -62,7 +62,7 @@ while len(possibleSpace) > 0:
         #If you want to script to stop after a game over
         # break
         # If you want the game to continue playing until it wins
-        click(pyautogui.locateOnScreen('dead.png', confidence=0.8))
+        click(pyautogui.locateOnScreen('dead.png'))
         possibleSpace = set(pyautogui.locateAllOnScreen('Untouched.png', confidence=0.8, region=reg))
         safe.clear()
         definite.clear()
@@ -74,7 +74,7 @@ while len(possibleSpace) > 0:
     possibleSpace = set(pyautogui.locateAllOnScreen('Untouched.png', confidence=0.8, region=reg))
     possibleDangers = list()
     for i in range(1, 9):
-        Is = set(pyautogui.locateAllOnScreen(str(i) + "2.png", confidence=0.8, region=reg))
+        Is = set(pyautogui.locateAllOnScreen(str(i) + ".png", confidence=0.8, region=reg))
         Is.difference_update(noUse[i-1])
         for num in Is:
             around = getAround(num)
@@ -97,14 +97,21 @@ while len(possibleSpace) > 0:
     if len(safe) == 0 and len(definite) == 0:
         print('logic')
         for i in possibleDangers:
-            new_stuff = []
             if len(i[0]) == 0:
                 continue
+            ToBeAdded = []
             for j in possibleDangers:
                 if len(j[0]) == 0:
                     continue
                 if i == j:
                     continue
+                if i[0] == j[0]:
+                    if i[2] - i[1] > j[2] - j[1]:
+                        possibleDangers.remove(i)
+                        break
+                    else:
+                        possibleDangers.remove(j)
+                        continue
                 if len(i[0].intersection(j[0])) > 0:
                     inter = i[0].intersection(j[0])
                     i_new = i[0].difference(j[0])
@@ -113,33 +120,31 @@ while len(possibleSpace) > 0:
                     must_atmost = min(i[2], j[2], len(inter))
                     IDanger = [i_new, max(i[1] - must_atmost, 0), min(i[2] - must_atleast, len(i_new))]
                     JDanger = [j_new, max(j[1] - must_atmost, 0), min(j[2] - must_atleast, len(j_new))]
+                    going = [inter, must_atleast, must_atmost]
+                    if going not in possibleDangers and len(going) > 0:
+                        possibleDangers.append(going)
                     if must_atleast == must_atmost:
+                        ToBreak = False
                         if i[2] == must_atleast:
                             safe.update(i_new)
                         elif i[1] - must_atleast == len(i_new):
                             definite.update(i_new)
                         elif IDanger not in possibleDangers and len(i_new) > 0:
-                            possibleDangers.append(IDanger)
+                            ToBeAdded.append(IDanger)
                         if j[2] == must_atleast:
                             safe.update(j_new)
                         elif j[1] - must_atleast == len(j_new):
                             definite.update(j_new)
                         elif JDanger not in possibleDangers and len(j_new) > 0:
-                            possibleDangers.append(JDanger)
-                        possibleDangers.remove(i)
-                        possibleDangers.remove(j)
-                        possibleDangers.append([inter, must_atleast, must_atmost])
-                        break
+                            ToBeAdded.append(JDanger)
                     else:
-                        going = [inter, must_atleast, must_atmost]
                         if IDanger not in possibleDangers and len(i_new) > 0:
-                            possibleDangers.append(IDanger)
+                            ToBeAdded.append(IDanger)
                         if JDanger not in possibleDangers and len(j_new) > 0:
-                            possibleDangers.append(JDanger)
-                        if going not in possibleDangers and len(going) > 0:
-                            possibleDangers.append(going)
+                            ToBeAdded.append(JDanger)
+            possibleDangers.extend(ToBeAdded)
     if len(safe) == 0 and len(definite) == 0 and len(possibleSpace) > 0:
-        # break
+        break
         print("the dice goes BRRRRRRRRRRRR")
         if len(possibleDangers) > 0:
             safe.add(random.choice(tuple(possibleDangers[random.randint(0, len(possibleDangers) - 1)][0])))
